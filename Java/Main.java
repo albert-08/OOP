dd4b69577e3005679710c3365049dfed0c55ff0c
Alberto Flores <alberto>
2021-05-30 16:44:22 -0500

    add polimorfismo

--- Java/Main.java
+++ Java/Main.java
@@ -2,12 +2,12 @@ class Main {
   public static void main(String[] args) {
     System.out.println("Hello world!");
 
-    Car car = new Car("AMQ123", new Account("Frida Benitez", "123456"));
-    car.passenger = 4;
-    car.printDataCar();
+    UberX uberX = new UberX("AMQ123", new Account("Frida Benitez", "123456"), "Chevrolet", "Sonic");
+    uberX.setPassenger(4);
+    uberX.printDataCar();
 
-    Car car2 = new Car("AMQ456", new Account("Daniela Valdez", "456789"));
-    car2.passenger = 4;
-    car2.printDataCar();
+    UberVan uberVan = new UberVan("AMQ456", new Account("Daniela Valdez", "456789"));
+    uberVan.setPassenger(6);
+    uberVan.printDataCar();
   }
 }
